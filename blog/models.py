791ce3417fef78fcbcd04642eacd6c7abaa05072
kalamaio <48555660+kalamaio@users.noreply.github.com>
2022-02-11 19:58:51 +0100

    Add __repr__ in models.py

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,8 @@ class User (db.Model):
     password = db.Column ( db.String(250), nullable = False )
     posts = db.relationship ( 'Post', backref = 'author', lazy = 'dynamic')
     
+    def __repr__(self):
+        return f"User('{self.id}', '{self.username}', '{self.emailk}')"
     
 class Post ( db.Model):
     id = db.Column ( db.Integer, primary_key = True)
@@ -19,3 +21,6 @@ class Post ( db.Model):
     title = db.Column ( db.String(120), nullable = False)
     description = db.Column ( db.String(240))
     body = db.Column ( db.Text(), nullable = False )
+    
+        def __repr__(self):
+        return f"Post ('{self.id}', '{self.title}')"
