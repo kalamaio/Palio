21756df5f14df8c75defee7140b38a50a8ce6695
kalamaio <48555660+kalamaio@users.noreply.github.com>
2022-02-11 20:04:26 +0100

    Add deff for import db user post in flask shell

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 import imp
-from blog import app
+from blog import app, db
+from blog.models import User, Post
+
+# Importa database e classi User Post nella shell di flask
+
+@app.shell_context_processor
+def meke_shell_context():
+    return {'db' : db, 'User' : User, 'Post' : Post}
