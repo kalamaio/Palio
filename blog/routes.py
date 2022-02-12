93a17e9ecf6466d8560f1f1a4d6a936112a91e1a
kalamaio <48555660+kalamaio@users.noreply.github.com>
2022-02-12 18:40:01 +0100

    Add home page post read the db query

--- blog/routes.py
+++ blog/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template
 # Importa istanza di flask avviata dentro __init__.py
 from blog import app
+# Importa istanza di Post
+from blog.models import Post
 
 # Definizione delle rotte del sito 
 @app.route ("/")
 def homepage ():
-    posts = [{"title": "Primo Post", "body": "Corpo del primo Post"},
-            {"title": "Secondo Post", "body": "Corpo del secondo Post"}]
+    posts = Post.query.order_by(Post.created_at.desc()).all()
     return render_template ('homepage.html', posts = posts)
 
 @app.route ('/about')
